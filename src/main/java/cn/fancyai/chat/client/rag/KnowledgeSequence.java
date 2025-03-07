74ac6943ca1eb464e950a5c82a0b6fc9f1706900
fancy H <fancy888@msn.com>
2025-03-07 09:31:45 +0800

    重构

--- src/main/java/cn/fancyai/chat/client/rag/KnowledgeSequence.java
+++ src/main/java/cn/fancyai/chat/client/rag/KnowledgeSequence.java
@@ -4,17 +4,23 @@ import com.aliyun.auth.credentials.Credential;
 import com.aliyun.auth.credentials.provider.StaticCredentialProvider;
 import com.aliyun.sdk.service.bailian20231229.AsyncClient;
 import darabonba.core.client.ClientOverrideConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 
 public abstract class KnowledgeSequence<T> {
     public static final String CATEGORY_ID = "cate_54146e8aa37a41ad90d215e5b7d67a36_11209711";
     public static final String WORKSPACE_ID = "llm-z6xbh2xfa9n1er6q";
 
+    @Value("${ai.ailibaba-accessKeyId}")
+    private String accessKeyId;
+    @Value("${ai.ailibaba-accessKeySecret}")
+    private String accessKeySecret;
+
     protected final AsyncClient asyncClient;
 
     public KnowledgeSequence() {
         StaticCredentialProvider provider = StaticCredentialProvider.create(Credential.builder()
-                .accessKeyId("LTAI5tAB1ToowbJh6Cop4CtF")
-                .accessKeySecret("xqLaiwr26TaPa33lwcmBEzkDvBGzhq")
+                .accessKeyId(accessKeyId)
+                .accessKeySecret(accessKeySecret)
                 .build());
         asyncClient = AsyncClient.builder().region("cn-beijing")
                 .credentialsProvider(provider).
